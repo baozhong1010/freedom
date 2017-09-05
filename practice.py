1970cd864398679f3e0ff91f3c3caba5d2f0f880
baozhong1010 <805062772@qq.com>
2017-09-05 16:48:01 +0800

    add practice.py2

--- practice.py
+++ practice.py
@@ -1,5 +1,5 @@
 d={'math': 88, 'chinese': 70, 'english': 80}
-def keys(data):
-    for i in data:
-        print i
-keys(d)
+def values():
+    for value in d.itervalues():
+        print value
+values()
