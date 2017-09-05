a43ad464a1e5471e7c23c3fa57be3ec35115a602
baozhong1010 <805062772@qq.com>
2017-09-05 16:07:00 +0800

    add practice.py

--- practice.py
+++ practice.py
@@ -1,8 +1,5 @@
 d={'math': 88, 'chinese': 70, 'english': 80}
 def keys(data):
-    mess = []
     for i in data:
-        mess.append(i)
-    return mess
-    print mess
-data = keys(d)
+        print i
+keys(d)
