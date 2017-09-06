7ab3f26810b52f5d9b1cc5edf4601f99dbf52547
baozhong1010 <805062772@qq.com>
2017-09-06 16:35:30 +0800

    add practice.py

--- practice.py
+++ practice.py
@@ -31,24 +31,20 @@ mess(d,'math')
 
 
 
-d={'math': {'math': 88, 'chinese': 70, 'english': 80}, 'chinese': 70, 'english': 80}
 d2 = {0: 'aaaa', 1: 'bbbb', 'asdf': 123456, 'fdsa': {'aaa': 111, 'bbb': 222, 'ccc': {'aaaa': 1111, 'bbbb': 2222}}, 'a list': [1, 2, 3, 4]}
 
-def mess(data,objkey):
+def mess(data):
     for i in data:
         value = data[i]
         _type = type(value)
-        if i == objkey:
+        if _type == dict:
             print i,value,_type
-            for j in data[objkey]:
+            for j in data['fdsa']:
                 value1 = value[j]
                 _type1 = type(value1)
                 print j,value1,_type1
         else:
             print i,value,_type
-mess(d,'math')
-print '-' * 20
-mess(d2, 'fdsa')
-
+mess(d2)
 
 
