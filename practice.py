7f27cb18e210440cfff5d4843c91b4fa2bda5ad7
baozhong1010 <805062772@qq.com>
2017-09-06 10:41:21 +0800

    add practice4

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
-d={'math': 88, 'chinese': 70, 'english': 80}
+'''
+d={'math': {'math': 88, 'chinese': 70, 'english': 80}, 'chinese': 70, 'english': 80}
 def values(data):
     for i in data:
         value = data[i]
@@ -6,5 +7,20 @@ def values(data):
         print i,value,_type
         
 values(d)
+'''
 
 
+
+
+
+
+d={'math': {'math': 88, 'chinese': 70, 'english': 80}, 'chinese': 70, 'english': 80}
+def mess(data,objkey):
+    for i in data:
+        value = data[i]
+        _type = type(value)
+        if i == objkey:
+            for j in data[objkey]:
+                print j
+        print i,value,_type
+mess(d,'math')
