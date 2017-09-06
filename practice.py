e46ad65ddfd49347bd727511508c40a77e84de4b
baozhong1010 <805062772@qq.com>
2017-09-06 11:47:39 +0800

    add practice4

--- practice.py
+++ practice.py
@@ -22,10 +22,9 @@ def mess(data,objkey):
         if i == objkey:
             for j in data[objkey]:
                 value1 = value[j]
-                _type = type(value1)
-                print j,value1,_type
-        else:
-            
-            print i,value,_type
+                _type1 = type(value1)
+                print j,value1,_type1
+                  
+        print i,value,_type
 mess(d,'math')
 
